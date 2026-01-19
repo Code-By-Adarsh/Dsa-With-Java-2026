72c516c89f39a830806fdf95055ab37f718b5656
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-01-19 23:52:04 +0530

    Add: Day-3

--- Day_03/Day03.java
+++ Day_03/Day03.java
@@ -1,2 +1,29 @@
 public class Day03 {
+    public static void main(String[] args) {
+
+        // 1st - BODMAS
+        // *,/,% > + and -
+        // In *,/ and % follows left-to-right
+
+        // 2nd - +=, -=, *=
+        // x += == x = x + 1
+        // x -= == x = x - 1
+        // x *= == x = x * 1
+
+        // 3rd - x++ , ++x , x-- , --x
+        int x = 10;
+        System.out.println(x++); // 10
+        System.out.println(x); // 11
+        System.out.println(++x); // 12
+
+        // 4rth
+        System.out.println(" ");
+        int x1 = 10;
+        int y1 = x1++;
+        System.out.println(x1+" "+y1);
+
+        // 5th - Relational Operator
+        System.out.println(" ");
+        System.out.println(6.0==6);
+    }
 }
