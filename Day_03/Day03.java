1083d8be838e39d007f80b5e8091d89bcdfd7811
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-01-20 16:39:50 +0530

    Add: Day-3

--- Day_03/Day03.java
+++ Day_03/Day03.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Day03 {
     public static void main(String[] args) {
 
@@ -25,5 +27,90 @@ public class Day03 {
         // 5th - Relational Operator
         System.out.println(" ");
         System.out.println(6.0==6);
+
+        // 6th - Even/Odd
+        System.out.println(" ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your positive number: ");
+        int x2 = sc.nextInt();
+        if (x2%2 == 0){
+            System.out.printf("%d is an even number.",x2);
+        }else {
+            System.out.printf("%d is a odd number.",x2);
+        }
+
+        // 7th - Positive number divisible by 5 or not
+        System.out.println(" ");
+        System.out.print("\nEnter your number: ");
+        int x3 = sc.nextInt();
+        if (x3%5 == 0){
+            System.out.println("Divisible by 5.");
+        }else {
+            System.out.println("Not divisible by 5.");
+        }
+
+        // 8th - Printing absolute integer of given number
+        System.out.println(" ");
+        System.out.print("Enter your number: ");
+        int x4 = sc.nextInt();
+        System.out.print("Absolute Integer: ");
+        if (x4>=0){
+            System.out.println(x4);
+        }else {
+            System.out.println(-x4);
+        }
+
+        // 9th - Real number is integer or not
+        System.out.println(" ");
+        System.out.print("Enter your number: ");
+        double x5 = sc.nextDouble();
+        if ((int)x5 == x5){
+            System.out.println("It is an integer.");
+        }else {
+            System.out.println("It is not integer.");
+        }
+
+        // 10th - Profit/Loss
+        System.out.println(" ");
+        System.out.print("Enter CP: ");
+        double x6 = sc.nextDouble();
+        System.out.print("Enter SP: ");
+        double y6 = sc.nextDouble();
+        if (x6 == y6){
+            System.out.println("No profit and no loss.");
+        } else if (x6 > y6) {
+            System.out.println("There is a loss of: "+(x6-y6));
+        }else {
+            System.out.println("There is a profit of: "+(y6-x6));
+        }
+
+        // --------- Logical Operator (&& / ||)----------------------
+        // 11th - 4 digit or not
+        System.out.println(" ");
+        System.out.print("Enter your (+)ve integer number: ");
+        int x7 = sc.nextInt();
+        if (x7 > 999 && x7 < 10000){
+            System.out.println("Given number is 4 digit number.");
+        }else {
+            System.out.println("Given number is not 4 digit number.");
+        }
+
+        // 12th - Magnitude of integer is smaller than 69 or not
+        System.out.println(" ");
+        System.out.print("Enter your integer: ");
+        int x8 = sc.nextInt();
+        if (x8>0){
+            if (x8<69){
+                System.out.println("True");
+            }else {
+                System.out.println("False");
+            }
+        }else {
+            if (-x8<69){
+                System.out.println("True");
+            }else {
+                System.out.println("False");
+            }
+        }
     }
 }
